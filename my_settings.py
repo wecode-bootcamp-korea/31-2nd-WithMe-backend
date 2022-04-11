6b20493ea21b62b7a869f387f6d4921c0ce42b51
leesan195159 <sniping1997@naver.com>
2022-04-11 16:05:36 +0900

    create: 초기세팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-6-gvhre)dp%0-f4ei5g+%r1dv@br=21lseu+4gja&&9lor(!-1'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'withme',
+        'USER': 'root',
+        'PASSWORD': '0000',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
