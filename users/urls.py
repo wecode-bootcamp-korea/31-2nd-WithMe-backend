e2ab9718be91331ba1288a801da96c2c9bc844fd
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-04-18 18:12:39 +0900

    modify 오타수정

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import KakaoLoginView
+from users.views import KakaoLoginView,PointChargeView
 
 urlpatterns = [
     path('/signin',KakaoLoginView.as_view()),
+    path('/point',PointChargeView.as_view()),
 ]
