77d8b948605b06ca67538b9bc4e0639bc56f206a
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-04-19 18:08:47 +0900

    modify

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from users.views import KakaoLoginView,PointChargeView
+from users.views import KakaoLoginView,UserInfomation
 
 urlpatterns = [
     path('/signin',KakaoLoginView.as_view()),
-    path('/point',PointChargeView.as_view()),
+    path('/userinfo',UserInfomation.as_view())
 ]
