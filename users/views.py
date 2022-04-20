27150e0d3ecd07ab20638462a194cff44817bc16
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-04-20 15:36:49 +0900

    conflict

--- users/views.py
+++ users/views.py
@@ -1,15 +1,12 @@
 import jwt, datetime, json
-
 from django.views import View
 from django.http  import HttpResponse, JsonResponse
-from django.conf         import settings
+from django.db.models import Q
 
-from users.models        import User
-from users.kakao         import KakaoAPI
+from users.models        import User,Host
 from places.models       import *
-
-from cores.decorator     import login_authorization
-
+from django.conf         import settings
+from users.kakao         import KakaoAPI
 
 from cores.decorator    import login_authorization
 
@@ -79,20 +76,44 @@ class ReservationView(View):
         
         return JsonResponse({'reservation_list':reservation_list}, status = 200)
 
+        
+class ReviewView(View):
     @login_authorization
     def post(self, request):
         user = request.user
         data = json.loads(request.body)
-        res = Reservation.objects.get(place_id = data['place_id'], user_id = user.id)
-
-        if res.place.running_date <= datetime.date.today():
-            return JsonResponse({"message" : "Reservations already used"}, status = 401)
-
-        res.delete()
-
-        price = Place.objects.get(id = data['place_id']).price
-        user.point += price
-        user.save()
         
-        return HttpResponse(status=200)
+        review , state = Review.objects.get_or_create(
+            user_id  = user.id,
+            place_id = Place.objects.get(title = data['title']).id,
+            defaults = {"content"  : data['review']}
+        )
+        if not data['review']:
+            return JsonResponse({'message' : 'Key error'}, status=400) 
 
+        if not state:
+            review.content = data['review']
+            review.save()
+
+        status =  201 if state else 200
+
+        return HttpResponse(status)
+
+    @login_authorization
+    def get(self, request):
+        user = request.user
+        
+        q  = Q(place__running_date__lte = datetime.date.today()) & Q(user_id = user.id) 
+
+        reservation_queryset = Reservation.objects.prefetch_related('place').filter(q)
+
+        result = [{
+            "id"          : reservation.id,
+            "running_date": reservation.place.running_date,
+            "title"       : reservation.place.title,
+            "sub_title"   : reservation.place.subtitle,
+            "image"       : reservation.place.image_url,
+            "review"      : [review.content for review in Review.objects.filter(place_id=reservation.place_id, user_id = user.id)]
+        }for reservation in reservation_queryset]
+
+        return JsonResponse({'review' : result}, status = 200)
