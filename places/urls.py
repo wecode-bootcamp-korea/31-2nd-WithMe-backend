83877a745f01841873cfbf37596cdee31e3f1b79
leesan195159 <sniping1997@naver.com>
2022-04-21 17:06:25 +0900

    Host place list 전달 기능 구현

--- places/urls.py
+++ places/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from places.views import PlaceInformationView, MainPlaceListView, PlaceReviewListView, PlaceHostInformationView, \
-                                                                                       PlaceSearchView
+                                                                  PlaceSearchView, HostResistPlaceList
 
 urlpatterns = [
     path('/placeinformation/<int:place_id>', PlaceInformationView.as_view()),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('/<int:place_id>/review', PlaceReviewListView.as_view()),
     path('/placehostinformation', PlaceHostInformationView.as_view()),
     path('/search', PlaceSearchView.as_view()),
-    path('/<int:place_id>/hostinformation', PlaceHostInformationView.as_view())
+    path('/<int:place_id>/hostinformation', PlaceHostInformationView.as_view()),
+    path('/hostregstlist', HostResistPlaceList.as_view())
 ]
